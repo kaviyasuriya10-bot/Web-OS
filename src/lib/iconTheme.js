74aa82c6325f184765020316d09a1c2b16cd350a
suriya “Sparky” <kaviyasuriya10@gmail.com>
2026-09-23 23:19:36 +0530

    Refactor getIconTheme function and add getIcon function

--- src/lib/iconTheme.js
+++ src/lib/iconTheme.js
@@ -3,3 +3,27 @@ export const ICON_THEMES = {
     Gryffindor: "/themes/Gryffindor/icons/",
     ravenclaw: "/themes/ravenclaw/icons/"
 };
+
+export function getIconTheme() {
+    return (
+        localStorage.getItem("webos-icon-theme") ||
+        "windows"
+    );
+}
+
+export function getIconTheme() {
+    return (
+        localStorage.getItem("webos-icon-theme") ||
+        "windows"
+    );
+}
+export function getIcon(iconName) {
+
+    const theme = getIconTheme();
+
+    return (
+        ICON_THEMES[theme].path +
+        iconName +
+        ".png"
+    );
+}
