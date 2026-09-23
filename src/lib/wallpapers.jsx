5bed0d3c6a3b12c1952b366254ef6445b9f26c69
suriya “Sparky” <kaviyasuriya10@gmail.com>
2026-09-23 22:28:43 +0530

    Update default wallpaper and add new wallpaper options

--- src/lib/wallpapers.jsx
+++ src/lib/wallpapers.jsx
@@ -1,14 +1,14 @@
 export const VIDEO_RE = /\.(mp4|webm|mov|m4v|ogv|ogg)(\?.*)?$/i;
 export const CUSTOM_PREFIX = "idb://";
 export const COLOR_PREFIX = "color:";
-export const DEFAULT_WALLPAPER = "/wizard-wallpapers/great-hall.svg";
+export const DEFAULT_WALLPAPER =
+  "/wallpapers/default.jpg";
+
 export const WALL_STILLS = [
-  "/wizard-wallpapers/great-hall.svg",
-  "/wizard-wallpapers/castle-night.svg",
-  "/wizard-wallpapers/forbidden-forest.svg",
-  "/wizard-wallpapers/astronomy-tower.svg",
-  "/wizard-wallpapers/potions-lab.svg",
-  "/wizard-wallpapers/library.svg",
+  "/wallpapers/default.jpg",
+  "/wallpapers/space.jpg",
+  "/wallpapers/nature.jpg",
+  "/wallpapers/city.jpg",
 ];
 export const WALL_LIVE = [];
 export const WALL_SOLIDS = ["#09070d","#17111f","#24152b","#3b1f35","#14233b","#5b3c1f","#e6d4a1"];
